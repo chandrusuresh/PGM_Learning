22a58530caf9cb9b7cf5ffe38f008352654f9cc4
Chandrasekar Sureshkumar <csureshkumar@roushfenway.com>
2019-02-11 18:48:06 -0500

    wk3 solutions to 1st 3 problems
    Solutions to Fittinng Gaussian, Linear Gaussian and computing log likelihood.

--- wk3/ComputeLogLikelihood.m
+++ wk3/ComputeLogLikelihood.m
@@ -28,3 +28,27 @@ loglikelihood = 0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i = 1:size(dataset,1)
+    prob = 0.0;
+    for j = 1:length(P.c)
+        pc = 0.0;
+        for k = 1:length(P.clg)
+            if G(k,1) == 0
+                pc = pc + lognormpdf(dataset(i,k,1),P.clg(k).mu_y(j),P.clg(k).sigma_y(j)) + ...
+                        + lognormpdf(dataset(i,k,2),P.clg(k).mu_x(j),P.clg(k).sigma_x(j)) + ...
+                        + lognormpdf(dataset(i,k,3),P.clg(k).mu_angle(j),P.clg(k).sigma_angle(j));
+            else
+                parent_val = dataset(i,G(k,2),:);
+                data_val   = dataset(i,k,:);
+                mu_y = sum(P.clg(k).theta(j,1:4).*[1,parent_val(:)']);
+                mu_x = sum(P.clg(k).theta(j,5:8).*[1,parent_val(:)']);
+                mu_angle = sum(P.clg(k).theta(j,9:12).*[1,parent_val(:)']);
+                pc = pc + lognormpdf(data_val(1,1,1),mu_y,P.clg(k).sigma_y(j)) + ...
+                          lognormpdf(data_val(1,1,2),mu_x,P.clg(k).sigma_x(j)) + ...
+                          lognormpdf(data_val(1,1,3),mu_angle,P.clg(k).sigma_angle(j));
+            end
+        end     
+        prob = prob + exp(log(P.c(j)) + pc);
+    end
+    loglikelihood = loglikelihood + log(prob);
+end
--- wk3/FitLinearGaussianParameters.m
+++ wk3/FitLinearGaussianParameters.m
@@ -27,21 +27,44 @@ sigma = 1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+A1 = zeros(N,N);
+A2 = zeros(1,N);
+for i = 1:N
+    A1(i,i) = mean(U(:,i).*U(:,i));
+    A2(1,i) = mean(U(:,i));
+    for j = i+1:N
+        A1(j,i) = mean(U(:,j).*U(:,i));
+        A1(i,j) = A1(j,i);
+    end
+end
+A = [A2,1;A1,A2'];
 
 % B = [ E[X]; E[X*U(1)]; ... ; E[X*U(n)] ]
-
 % construct B
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+B = zeros(N+1,1);
+B(1) = mean(X);
+for i = 2:length(B)
+    B(i) = mean(X.*U(:,i-1));
+end
 
 % solve A*Beta = B
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Beta = A\B;
 
 % then compute sigma according to eq. (11) in PA description
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sigma = (std(X,1))^2;
+for i = 1:N
+    for j = 1:N
+        cov_Ui_Uj = A1(i,j) - A2(i)*A2(j);
+        sigma = sigma - Beta(i)*Beta(j)*cov_Ui_Uj;
+    end
+end
+sigma = sqrt(sigma);
