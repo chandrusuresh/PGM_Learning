78cb31068daeb2ad3baa4f981dc37556c9e357bf
Chandrasekar Sureshkumar <csureshkumar@roushfenway.com>
2019-02-12 18:14:22 -0500

    SOlution for Qn-5
    Code for Classification and prediction

--- wk3/ClassifyDataset.m
+++ wk3/ClassifyDataset.m
@@ -16,8 +16,25 @@ function accuracy = ClassifyDataset(dataset, labels, P, G)
 N = size(dataset, 1);
 accuracy = 0.0;
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+class = length(P.c);
+prediction = zeros(size(labels));
+for k = 1:size(dataset,1)
+    log_prob_ObsGClass = zeros(1,class);
+    prob_Obs_and_Class = zeros(1,class);
+    for i = 1:class
+        log_prob_ObsGClass(i) = ComputeProbability_Obs_given_class(P,G,dataset(k,:,:),i);
+        prob_Obs_and_Class(i) = log(P.c(i)) + log_prob_ObsGClass(i);
+    end
+    ind = find(prob_Obs_and_Class == max(prob_Obs_and_Class));
+    prediction(k,ind) = 1;
+end
+
+diff_ind = find(prediction(:,1)-labels(:,1) == 0);
+accuracy = length(diff_ind)/size(labels,1);
 
 fprintf('Accuracy: %.2f\n', accuracy);
