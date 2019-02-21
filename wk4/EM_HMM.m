dd6cc1322691b5562cc25dcdf49015d9cb905b71
Chandrasekar Sureshkumar <csureshkumar@roushfenway.com>
2019-02-20 19:33:32 -0500

    Partial solution to problem-2
    Solution to EM of HMM

--- wk4/EM_HMM.m
+++ wk4/EM_HMM.m
@@ -40,10 +40,13 @@ PairProb = InitialPairProb;
 loglikelihood = zeros(maxIter,1);
 
 P.c = [];
-P.clg.sigma_x = [];
+P.clg.mu_y = [];
 P.clg.sigma_y = [];
+P.clg.mu_x = [];
+P.clg.sigma_x = [];
+P.clg.mu_angle = [];
 P.clg.sigma_angle = [];
-
+P.clg.theta = [];
 % EM algorithm
 for iter=1:maxIter
   
@@ -54,11 +57,34 @@ for iter=1:maxIter
   % Hint: This part should be similar to your work from PA8 and EM_cluster.m
   
   P.c = zeros(1,K);
-  
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
-  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  cProb = zeros(length(actionData),K);
+  for i = 1:length(actionData)
+      cProb(i,:) = cProb(i,:) + ClassProb(actionData(i).marg_ind(1),:);
+  end
+  cProb = mean(cProb,1);
+
+  for k = 1:K
+    P.c(k) = mean(cProb(:,k));%mean(ClassProb(:,k));%
+    for i = 1:size(G,1)
+      if G(i,1) == 0
+          [P.clg(i).mu_y(k),P.clg(i).sigma_y(k)] = FitG(poseData(:,i,1),ClassProb(:,k));
+          [P.clg(i).mu_x(k),P.clg(i).sigma_x(k)] = FitG(poseData(:,i,2),ClassProb(:,k));
+          [P.clg(i).mu_angle(k),P.clg(i).sigma_angle(k)] = FitG(poseData(:,i,3),ClassProb(:,k));
+      else
+          parent_data = reshape(poseData(:,G(i,2),:),[size(poseData,1),size(poseData,3)]);
+          class_data = reshape(poseData(:,i,:),[size(poseData,1),size(poseData,3)]);
+          [theta1,P.clg(i).sigma_y(k)] = FitLG(class_data(:,1),parent_data,ClassProb(:,k));
+          [theta2,P.clg(i).sigma_x(k)] = FitLG(class_data(:,2),parent_data,ClassProb(:,k));
+          [theta3,P.clg(i).sigma_angle(k)] = FitLG(class_data(:,3),parent_data,ClassProb(:,k));
+          P.clg(i).theta(k,1:4) = [theta1(4),theta1(1:3)'];
+          P.clg(i).theta(k,5:8) = [theta2(4),theta2(1:3)'];
+          P.clg(i).theta(k,9:12) = [theta3(4),theta3(1:3)'];
+      end
+    end
+  end  
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   % M-STEP to estimate parameters for transition matrix
@@ -72,20 +98,29 @@ for iter=1:maxIter
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  TM = sum(PairProb,1);
   
+  P.transMatrix = P.transMatrix + reshape(TM,[3,3]);
+  P.transMatrix = P.transMatrix./sum(P.transMatrix,2);
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  
-    
+
   % E-STEP preparation: compute the emission model factors (emission probabilities) in log space for each 
   % of the poses in all actions = log( P(Pose | State) )
   % Hint: This part should be similar to (but NOT the same as) your code in EM_cluster.m
   
   logEmissionProb = zeros(N,K);
-  
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  
+  currDir = cd;
+  altDir = '../wk3/';
+  for i = 1:size(poseData,1)
+      cd(altDir);
+      for class = 1:K
+        logEmissionProb(i,class) = ComputeProbability_Obs_given_class(P,G,poseData(i,:,:),class);
+      end
+      cd(currDir);
+  end
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
     
@@ -95,7 +130,6 @@ for iter=1:maxIter
   % Also compute log likelihood of dataset for this iteration
   % You should do inference and compute everything in log space, only converting to probability space at the end
   % Hint: You should use the logsumexp() function here to do probability normalization in log space to avoid numerical issues
-  
   ClassProb = zeros(N,K);
   PairProb = zeros(V,K^2);
   loglikelihood(iter) = 0;
@@ -103,7 +137,39 @@ for iter=1:maxIter
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+  pc = zeros(1,length(actionData));
+  for i = 1:length(actionData)
+      numStates = length(actionData(i).marg_ind);
+      CTree = [struct('var',[1],'card',[K],...
+                        'val',log(P.c) + logEmissionProb(actionData(i).marg_ind(1),:))];
+      for j = 2:numStates
+          CTree(j) = struct('var',[j,j-1],'card',[K,K],...
+                        'val',zeros(1,K*K));
+          val = CTree(j).val;
+          for k1 = 1:K
+              st = (k1-1)*K + 1;
+              ed = k1*K;
+              val(st:ed) = log(P.transMatrix(k1,:)) + logEmissionProb(actionData(i).marg_ind(j),:);
+          end
+          CTree(j).val = val;
+      end
+      [M, PCalibrated] = ComputeExactMarginalsHMM(CTree);
+      
+      for j = 1:length(M)
+        ClassProb(actionData(i).marg_ind(j),:) = exp(M(j).val);
+        if j > 1
+            pairProb1 = PCalibrated.cliqueList(j-1).val - logsumexp(PCalibrated.cliqueList(j-1).val);
+            PairProb(actionData(i).pair_ind(j-1),:) = exp(pairProb1);
+        end
+      end
+      pc1 = zeros(1,K);
+      for ii = 1:length(M)
+          pc1 = pc1 + M(ii).val;
+      end
+      pc(i) = logsumexp(pc1);
+      CTree = [];
+  end
+  keyboard
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   % Print out loglikelihood
